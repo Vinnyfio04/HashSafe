3200c4e0fe283993264e23e58efe1f23281f730a
zdg5063-glitch <zdg5063@psu.edu>
2026-05-02 01:50:59 -0400

    fix

--- server/routers/auth.mjs
+++ server/routers/auth.mjs
@@ -1,5 +1,5 @@
 import express from "express";
-import User from "../../models/users.model.js";
+import User from "../models/users.model.js";
 const router = express.Router();
 
 // POST /auth/register – Creates an account for a user
